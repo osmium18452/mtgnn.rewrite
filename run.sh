221af74152cc5cd35031a0e5696bdbfe918087cf
17110 <1711042219@qq.com>
2023-07-27 21:44:00 +0800

    fixed some bugs

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-output_list="720 180 360 320 192 160 96 80 48 40 24 20"
+output_list="720 480 360 320 192 160 96 80 48 40 24 20"
 dataset_list="gweather etth1 etth2 ettm1 ettm2 exchange wht finance"
 batch_size=256
 stride=1
@@ -12,7 +12,7 @@ cuda="3"
 for dataset in $dataset_list; do
     for output_len in $output_list; do
         echo "\033[35mmodel: dataset: $dataset_count/8, output len: $output_count/12\033[0m"
-        save_file="save/best/$exec_date/$dataset/$output_len"
+        save_file="save/$exec_date/best/$dataset/$output_len"
         exec="python main.py --fudan -GBDC $cuda -e $epochs -o $output_len -b $batch_size --fixed_seed 3407 -d $dataset -S $save_file --draw --stride $stride"
         echo $exec
         if [ ! -e $save_file/result.json ]; then
@@ -20,7 +20,7 @@ for dataset in $dataset_list; do
         else
             echo 'file exists'
         fi
-        save_file="save/worst/$exec_date/$dataset/$output_len"
+        save_file="save/$exec_date/worst/$dataset/$output_len"
         exec="python main.py --fudan -GDC $cuda -e $epochs -o $output_len -b $batch_size --fixed_seed 3407 -d $dataset -S $save_file --stride $stride"
         echo $exec
         if [ ! -e $save_file/result.json ]; then
@@ -37,7 +37,7 @@ done
 output_list="24 36 48 60"
 dataset="ill"
 for output_len in $output_list; do
-    save_file="save/best/$exec_date/$dataset/$output_len"
+    save_file="save/$exec_date/best/$dataset/$output_len"
     exec="python main.py --fudan -GBDC $cuda -e $epochs -o $output_len -b $batch_size --fixed_seed 3407 -d $dataset -S $save_file --draw --stride $stride"
     echo $exec
     if [ ! -e $save_file/result.json ]; then
@@ -45,7 +45,7 @@ for output_len in $output_list; do
     else
         echo 'file exists'
     fi
-    save_file="save/worst/$exec_date/$dataset/$output_len"
+    save_file="save/$exec_date/worst/$dataset/$output_len"
     exec="python main.py --fudan -GDC $cuda -e $epochs -o $output_len -b $batch_size --fixed_seed 3407 -d $dataset -S $save_file --stride $stride"
     echo $exec
     if [ ! -e $save_file/result.json ]; then
